5f94c1b81a1665c386d2b9897df692b793c07d51
chillaranand <anand21nanda@gmail.com>
2017-04-18 20:15:37 +0530

    Setup chamanti in a virtualenv environment

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 echo "Installing system dependencies..."
-sudo apt-get install libffi-dev
+sudo apt-get install -y libffi-dev
 
 echo "Installing python packages..."
-sudo pip3 install --upgrade pip
-sudo pip3 install -r requirements.txt
+
+sudo -H pip install virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv chamanti_ocr -p /usr/bin/python3.5
+workon chamanti_ocr
+
+pip install --upgrade pip
+pip install -r requirements.txt
